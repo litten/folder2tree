f29382024478dffa19cebb346ab985833fb37c9e
HJava <554319139@qq.com>
2014-08-14 09:33:12 +0800

    change '==' to '===',change author and version,format code

--- folder2tree.js
+++ folder2tree.js
@@ -1,27 +1,27 @@
 /**
- * 
- * @fileoverview Show your folder structure with tree nodes. 用树形节点展示文件夹结构 
- * @version 1.0
- * @author javahuang, litten
- * @lastUpdate 2014-07-28 10:40 
+ *
+ * @fileoverview Show your folder structure with tree nodes. 用树形节点展示文件夹结构
+ * @version 1.1
+ * @author HJava, litten
+ * @lastUpdate 2014-07-28 10:40
  * @function: 1. init(ctn ,data)
  */
-(function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
+(function(root, factory) {
+    if(typeof define === 'function' && define.amd) {
         define([], factory);
     } else {
         root['folder2tree'] = factory();
     }
-}(this, function () {
+}(this, function() {
 
-    var arr2obj = function(arr){
+    var arr2obj = function(arr) {
         var obj = {};
-        for(var i=0,len=arr.length; i<len; i++){
+        for(var i = 0, len = arr.length; i < len; i++) {
             var em = arr[i];
-            if(typeof(em) === "string" || typeof(em) == "number"){
+            if(typeof(em) === "string" || typeof(em) === "number") {
                 obj[em] = em;
-            }else if(typeof(em) === "object"){
-                for(var key in em){
+            } else if(typeof(em) === "object") {
+                for(var key in em) {
                     obj[key] = arr2obj(em[key]);
                 }
             }
@@ -53,11 +53,11 @@
 
         for(var i in data) {
             var front = content;
-            var isLast = (count == len) ? true : false;
+            var isLast = (count === len) ? true : false;
             front += isLast ? '&emsp;&nbsp;&nbsp;' : '│&emsp;';
             if(typeof data[i] === 'string' || typeof data[i] === 'number') {
                 all.push(createDiv(i, data[i], content, isLast));
-            } else if(typeof data[i] == "object") {
+            } else if(typeof data[i] === "object") {
                 all.push(createDiv(i, i, content, isLast));
                 var arr = arguments.callee(data[i], front);
                 var parent = createDiv(i, '', content, isLast);
@@ -97,7 +97,7 @@
         init: function(ctn, data) {
             ctn = ctn || document.getElementsByTagName("body")[0];
             if(data) {
-                if(data instanceof Array){
+                if(data instanceof Array) {
                     data = arr2obj(data);
                 }
                 console.log(data);
